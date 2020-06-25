4af93e59251debe9346ab45e2da30a4ec05d8a6f
Isha <Isha@DESKTOP-0MP3IBM>
2020-06-26 01:26:20 +0530

    first commit under order service new

---
+++ OrderServiceNew/src/main/java/com/example/OrderServiceNew/OrderServiceNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrderServiceNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceNewApplication.class, args);
+	}
+
+}
---
+++ OrderServiceNew/src/test/java/com/example/OrderServiceNew/OrderServiceNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrderServiceNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
